e94099c62b69f5e46a6351736859d6c68b28e3ae
flintaivan <flinta.ivan@gmail.com>
2023-01-23 15:29:55 +0000

    Add register routes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,7 +9,7 @@ class RegisterController extends Controller
 {
     public function show()
     {
-        return view('registration');
+        return view('auth.registration');
     }
 
     public function validateRegistration(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', [RegisterController::class, 'show'])->name('register.show');
+Route::post('register_validate', [RegisterController::class, 'validateRegistration'])->name('register.validate');
